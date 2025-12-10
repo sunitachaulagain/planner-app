b260503d319b362a14f8b2bbc009873f806e8a8b
sunita chaulagain <sunitachaulagain300@gmail.com>
2025-12-10 18:42:07 +0545

    modifies signup and profile view

--- plans/views.py
+++ plans/views.py
@@ -7,20 +7,28 @@ import datetime
 
 from .forms import SignUpForm, LoginForm, CategoryForm, PlanForm, TaskForm, ProfileForm
 from .models import Profile, Category, Plan, Task
+from django.contrib.auth import login
+from .models import Profile
 
 # ===========================
 # AUTH
 # ===========================
+
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
+            
+            # Ensure profile exists
+            Profile.objects.get_or_create(user=user)
+            
             login(request, user)
             return redirect('dashboard')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 
 def login_view(request):
@@ -238,5 +246,5 @@ def toggle_task_complete(request, task_id):
 # ===========================
 @login_required
 def profile_view(request):
-    profile = request.user.profile
+    profile, created = Profile.objects.get_or_create(user=request.user)
     return render(request, 'plans/profile.html', {'profile': profile})
